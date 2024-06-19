2470714521719dfea7d2d62f37e7b1db6442dfbb
OmerCanInan <142689387+OmerCanInan@users.noreply.github.com>
2024-06-19 03:56:45 +0300

    Tycoon_Game

--- Tycoon_Game/scripts/Login.gd
+++ Tycoon_Game/scripts/Login.gd
@@ -1,7 +1,5 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
@@ -9,7 +7,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
+	
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/Game.tscn")
+	if $NinePatchRect2/username_edit.text == "1234" && $NinePatchRect2/password_edit.text == "1234":
+		get_tree().change_scene_to_file("res://scenes/Game.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
